70c71a0d681d76791b922c49b5831e10244bfd8a
p1508389 <florent.tirvaudey@etu.univ-lyon1.fr>
2020-03-31 17:57:19 +0200

    intersection rectangles fixes

--- Test/src/VueController/Main.java
+++ Test/src/VueController/Main.java
@@ -8,6 +8,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import javafx.scene.paint.Color;
@@ -15,6 +16,8 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.layout.GridPane;
 
 import java.io.FileInputStream;
+import java.util.Observable;
+import java.util.Observer;
 
 
 public class Main extends Application {
@@ -24,7 +27,56 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        StackPane root = new StackPane();
+        Route route = new Route();
+        GridPane grid = new GridPane();
+        ImageView[][] tabCase = new ImageView[SIZE_X][SIZE_Y];
+
+        Image vert = new Image(new FileInputStream("src/img/vert.png"));
+        ImageView imageVert = new ImageView(vert);
+
+        Image gris = new Image(new FileInputStream("src/img/gris.png"));
+        ImageView imageGris = new ImageView(gris);
+
+        imageGris.setX(46);
+        imageGris.setY(45);
+
+        for(int i = 0; i < SIZE_X; i++) {
+            for(int j = 0; j < SIZE_Y; j++) {
+
+              /*  // AVEC les images (pas encore au point)
+                if(route.tabCase[i][j] instanceof Goudron) {
+                    tabCase[i][j].setImage(imageVert);
+                } else {
+                    image = imageGris;
+                }
+                grid.add(image, i, j); */
+
+
+                // SANS les images
+                Rectangle rectangle = new Rectangle();
+                rectangle.setX(46);
+                rectangle.setY(45);
+                rectangle.setWidth(100);
+                rectangle.setHeight(100);
+                if(route.tabCase[i][j] instanceof Goudron) {
+                    rectangle.setFill(Color.GRAY);
+                } else {
+                    rectangle.setFill(Color.GREEN);
+                }
+                grid.add(rectangle, i, j);
+            }
+        }
+
+        // initialisation de la grille sans les images
+        for (int i = 0; i < SIZE_X; i++) {
+            for (int j = 0; j < SIZE_Y; j++) {
+                ImageView img = new ImageView();
+
+                grid.add(img, i, j);
+            }
+        }
 
         // définit la largeur et la hauteur de la fenêtre
         // en pixels, le (0, 0) se situe en haut à gauche de la fenêtre
@@ -34,28 +86,12 @@ public class Main extends Application {
         stage.setTitle("POPOPO POM");
 
         // la racine du sceneGraph est le root
-        Group root2 = new Group();
-        Scene scene = new Scene(root2);
-        scene.setFill(Color.SKYBLUE);
+      //  Group root2 = new Group();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
 
-        Route route = new Route();
-
-        GridPane grid = new GridPane();
-
-        Image vert = new Image(new FileInputStream("src/img/vert.png"));
-        ImageView imageVert = new ImageView(vert);
-
-        // création du soleil
-
-        //Circle sun = new Circle(60, Color.web("yellow", 0.8));
-        //sun.setCenterX(600);
-        //sun.setCenterY(100);
-
-        Image soleil = new Image(new FileInputStream("src/img/soleil.png"));
-        ImageView imageSoleil = new ImageView(soleil);
-
-        imageSoleil.setX(60);
-        imageSoleil.setY(50);
+        //imageSoleil.setX(60);
+        //imageSoleil.setY(50);
 
         // création du sol
         Rectangle ground = new Rectangle(0, 400, 800, 200);
@@ -66,14 +102,22 @@ public class Main extends Application {
         // le repère utilisé est celui du panneau
         Rectangle panel = new Rectangle( 0, -50, 500, 110);
         panel.setFill(Color.DARKBLUE);
-        // composer l'élément plus complexe
 
+
+        @Deprecated
+        Observer à = new Observer() {
+            @Override
+            public void update(Observable o, Object arg) {
+
+            }
+        };
 
         // ajout de tous les éléments de la scène
-        root2.getChildren().add(imageSoleil);
-        root2.getChildren().add(ground);
+        //root2.getChildren().add(imageSoleil);
+        //root2.getChildren().add(ground);
         //root.getChildren().add(sign);
-
+        root.requestFocus();
+        root.getChildren().add(grid);
         // ajout de la scène sur l'estrade
         stage.setScene(scene);
         // ouvrir le rideau
